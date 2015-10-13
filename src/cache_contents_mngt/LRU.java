24347fea15e3a04312dc1cc9d553c2cdeb5110bd
Jerôme Lemaire <jejelemaire@gmail.com>
2015-10-13 17:40:11 +0200

    LRU

--- src/cache_contents_mngt/LRU.java
+++ src/cache_contents_mngt/LRU.java
@@ -1,29 +1,64 @@
 package cache_contents_mngt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LRU extends Cache {
 
-	LinkedList<String> linkedlist;
+	LinkedList<Request> linkedlist;
 
 	LRU(int capacity, Boolean capacityInBytes, int warmup) {
 		super(capacity, capacityInBytes, warmup);
 		// Logic Custom
-		linkedlist = new LinkedList<String>();
+		linkedlist = new LinkedList<Request>();
 
 	}
 
-
-	public void get(String url, int size) {
-		super.get(url, size);
+	
+	public void get(String url, int size){
+		Request rqst = new Request(url,size);
+		if(this.capacityInBytes){
+			getTache2(rqst);
+		}
+		else{
+			getTache1(rqst);
+		}
+	}
+	
+	public void getTache1(Request rqst) { // changer de nom
+		
+		
+		// Si objet present (identiquement) dans le cache 
+		if(linkedlist.contains(rqst)){
+			linkedlist.remove(rqst);
+			linkedlist.addFirst(rqst);
+		}
+		// objet pas present dans le cache et cache pas au max
+		else if(linkedlist.size()<=this.capacity){ 
+			linkedlist.addFirst(rqst);
+		}
+		// objet pas present dans le cache et cache plein
+		else {
+			linkedlist.removeLast();
+			linkedlist.addFirst(rqst);
+		}
+	}
+	
+	public void getTache2(Request rsqt) {
+		
+		
+		
 	}
 
-	/*List<String> getCacheContent() {
-		return new ArrayList<String>(Arrays.asList(
-				"/history/apollo/",
-				"/shuttle/countdown/",
-				"/shuttle/missions/sts-73/mission-sts-73.html"
-		));
-	}*/
+	public List<String> getCacheContent() {
+		ArrayList<String> stringCache = new ArrayList<String>();
+		while(linkedlist.size() > 0){
+			Request tampon = linkedlist.removeLast();
+			stringCache.add(tampon.getUrl());
+		}
+		return stringCache;	
+	}
 }
 
